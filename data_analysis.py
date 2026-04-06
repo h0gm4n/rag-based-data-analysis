35709271ce16c9d50f1d21f8a3b879625f2b8668
h0gm4n <atte.hogman@gmail.com>
2026-04-06 21:57:44 +0300

    modify polars call

--- data_analysis.py
+++ data_analysis.py
@@ -6,6 +6,6 @@ import polars as pl
 # path = kagglehub.dataset_download("vivek468/superstore-dataset-final")
 # print("Path to dataset files:", path)
 
-data = pl.read_csv("superstore.csv", separator=",", encoding="utf8-lossy")
+data = pl.read_csv("data/superstore.csv", separator=",", encoding="utf8-lossy")
 print(data.shape)
 print(data[0])
